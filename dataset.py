d3dabe5d1e1f52f16a5bcee8ded19ca0030d26cc
Anya-gh <Aniket.Singh@warwick.ac.uk>
2023-04-09 13:01:45 +0100

    Dataset
    based on Ollie's dataset

--- dataset.py
+++ dataset.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 from extract import getMapping
 import pretty_midi
+from torch.utils.data.sampler import SubsetRandomSampler, SequentialSampler
 
 class MMGDataset(Dataset):
     def __init__(self, data_cfg):
@@ -34,6 +35,29 @@ class MMGDataset(Dataset):
     def __getitem__(self, index):
         robotic_path, performance_path = self.data.iloc[index]
         mapping = getMapping(pretty_midi.PrettyMIDI(performance_path), pretty_midi.PrettyMIDI(robotic_path))
+        # don't include pitch
         robotic_notes = np.delete(mapping[:,0], -1, axis=1)
         performance_notes = np.delete(mapping[:,1], -1, axis=1)
         return torch.tensor(robotic_notes), torch.tensor(performance_notes)
+
+def load_data(data_cfg: dict):
+
+    dataset = MMGDataset(data_cfg)
+
+    # Create splits
+    indices = list(range(len(dataset)))
+    if data_cfg.get("shuffle"):
+        np.random.shuffle(indices)
+
+    train_prop, val_prop, test_prop = data_cfg.get("dataset_split")
+    train_split = int(np.floor(train_prop * len(dataset)))
+    val_split = train_split + int(np.floor(val_prop * len(dataset)))
+    train_indices, val_indices, test_indices = indices[:train_split], indices[train_split:val_split], indices[val_split:]
+
+    batch_size = data_cfg.get("batch_size")
+
+    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_indices))
+    val_loader = DataLoader(dataset, batch_size=batch_size, sampler=SequentialSampler(val_indices))
+    test_loader = DataLoader(dataset, batch_size=batch_size, sampler=SequentialSampler(test_indices))
+
+    return train_loader, val_loader, test_loader
